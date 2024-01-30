cb56ffe55e01eea4daa3a014fe7b939bfdc3871f
ömer <ozcanomer15429@gmail.com>
2024-01-30 23:30:31 +0300

    upload v0.6

--- config.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-email = "enter email"
-pw = "password"
-username = "username"
-
-
-images_path = os.path.dirname(__file__)+"/Assets/Unposted"
-logInfo_path = os.path.dirname(__file__)+"/Assets/Procces_info.txt"
--- main.py
+++
@@ -1,8 +0,0 @@
-
-import config
-from bot import Bot
-
-TwitterBot = Bot(config.email,config.pw,config.username)
-TwitterBot.login()
-TwitterBot.post_multiple_image(config.images_path, "", config.logInfo_path)
-TwitterBot.logout()
